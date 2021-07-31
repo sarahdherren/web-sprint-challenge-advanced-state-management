b8fb3d5eb18fdaf7e110d1ce59371a58f8d687a7
Sarah D Herren <herren.sarah@icloud.com>
2021-07-31 10:55:23 -0500

    created reducers

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from "redux";
+import thunk from 'redux-thunk';
 
 import "./index.css";
 import App from "./App";
+import reducers from "./reducers";
 
 const { worker } = require('./mocks/browser');
 worker.start();
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,22 +1,56 @@
 
 export const initialState = {
+    smurfs: [],
+    isLoading: false,
+    error: ""
 }
 
-const reducer = ()=>{
+const reducer = (state = initialState, action)=>{
+
+    switch(action.type) {
+        case FETCH_SMURFS:
+            return {
+                ...state,
+                smurfs: [action.payload]
+            }
+        case API_FETCH:
+            return {
+                ...state,
+                isLoading: true
+            }
+        case API_FAIL:
+            return {
+                ...state,
+                isLoading: false,
+                error: `Our village seems to be out gathering smurf-berries, please try again later, ${action.payload}`
+            }
+        case NEW_SMURF:
+            const newSmurf = {
+                id: new Date(),
+                name: action.payload.name,
+                nickname: action.payload.nickname,
+                position: action.payload.position,
+                summary: action.payload.summary
+            }
+            return {
+                ...state,
+                smurfs: [state.smurfs, newSmurf]
+            }
+    }
 }
 
 //**************DO NOT EDIT ANY CODE BEYOND THIS POINT**************//
 export default reducer;
 
 //Task List:
-//1. Adds the following state values into the initialState:
-//  - an array of smurfs
-//  - a boolean indicating if the app is loading
-//  - a string indicating a possible error message
+//X Adds the following state values into the initialState:
+//  X an array of smurfs
+//  X a boolean indicating if the app is loading
+//  X a string indicating a possible error message
 
-//2. Add in the arguments needed to complete a standard reducer function.
-//3. Add in a reducer case to accomidate the start of a smurf fetch.
-//4. Add in a reducer case to accomidate the successful smurf api fetch.
-//5. Add in a reducer cases to accomidate the failed smurf api fetch.
-//6. Add in a reducer case to accomidate adding a smurf (including the name, nickname, position, summary and an internally generated id) into your smurf list.
-//7. Add in a reducer case that adds in a value to the error message.
+//X Add in the arguments needed to complete a standard reducer function.
+//X Add in a reducer case to accomidate the start of a smurf fetch.
+//X Add in a reducer case to accomidate the successful smurf api fetch.
+//X Add in a reducer cases to accomidate the failed smurf api fetch.
+//X Add in a reducer case to accomidate adding a smurf (including the name, nickname, position, summary and an internally generated id) into your smurf list.
+//X Add in a reducer case that adds in a value to the error message.
